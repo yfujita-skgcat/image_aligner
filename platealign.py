9c08a0200fe478c2211564f07c5ff4aed8361fdd
Yoshihiko Fujita <yoshihiko.fujita@cira.kyoto-u.ac.jp>
2018-04-08 02:04:18 +0900

    コマンドラインからデバッグできるようにした.

--- platealign.py
+++ platealign.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 '''
 Copyright (C) 2017 Matyas Kocsis, matyilona@openmailbox.org
 
@@ -32,6 +33,12 @@ import numpy as np
 import math
 import logging
 
+from lxml import etree
+import re
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
 #a logger for debugging/warnings
 logger = logging.getLogger( "platealign" )
 # logger.setLevel( 'WARNING' )
@@ -46,6 +53,17 @@ fh.setFormatter(formatter)
 logger.addHandler( fh )
 logger.debug( "Logger initialized" )
 
+# def new_affect(self, args=sys.argv[1:], output=True):
+#     """Affect an SVG document with a callback effect"""
+#     self.svg_file = args[-1]
+#     self.getoptions(args)
+#     self.parse()
+#     self.getposinlayer()
+#     self.getselected()
+#     self.getdocids()
+#     self.effect()
+#     if output: self.output()
+
 class ImageAlignEffect( inkex.Effect ):
     """
     Either draw marks for alignment, or align images
@@ -59,15 +77,105 @@ class ImageAlignEffect( inkex.Effect ):
         self.OptionParser.add_option( '-a', '--angle', action = 'store',
           type = 'int', dest = 'angle', default = '0',
           help = 'Rotate images.' )
-        self.OptionParser.add_option( '-o', '--od', action = 'store',
+        self.OptionParser.add_option( '-d', '--direction', action = 'store',
           type = 'string', dest = 'direction', default = 'horizontal',
           help = 'Set direction of the each filter image.' )
+        self.OptionParser.add_option("-s", "--selectedonly",
+            action="store", type="inkbool", 
+            dest="selectedonly", default=False,
+            help="Align only selected images")
           # help = 'Id of object to be aligned (use groups for multipe)' )
         # self.OptionParser.add_option( '-n', '--markernum', action = 'store',
         #   type = 'int', dest = 'markernum', default = '1',
         #   help = 'No. of images to align' )
 
     color_list = [ '#0000ff', '#00ff00', '#ff0000', '#ff00ff', '#00ffff', '#ffff00', '#8800ff', '#0088ff', '#00ff88', '#ff0088' ] 
+
+    # 多分このeffect()が affect() で呼ばれるんだと思う.
+    def effect( self ):
+
+        if self.options.selectedonly:
+            image_ids = self.options.ids
+        else:
+            image_ids = map(lambda x: x.get("id"), self.document.getroot().xpath('//svg:image', namespaces=inkex.NSS))
+
+        # self.document=document #not that nice... oh well
+        # path = '//svg:image'
+        # for node in self.document.getroot().xpath(path, namespaces=inkex.NSS):
+        #     self.embedImage(node)
+        logger.debug("image_ids = " + str(image_ids))
+
+        images = {}
+
+        image_array = {}
+
+        for img_id in image_ids:
+            img_obj = self.getElementById(img_id)
+            # img_obj.set("x", "0")
+            # logger.debug(str(etree.tostring(img_obj)))
+            # logger.debug(img_obj.get("id"))
+            # logger.debug("inkex.NSS = " + str(inkex.NSS))
+            for attr in img_obj.keys():
+                # logger.debug("attr = " + attr)
+                if re.search("href$", attr):
+                    # logger.debug("MATCH attr = " + attr)
+                    # filename = re.sub("^[^/]", "", img_obj.get(attr))
+                    filename = img_obj.get(attr).split("/")[-1]
+                    break
+
+            # logger.debug(str(img_obj.attrib))
+            # logger.debug(str(img_obj.get("{http://www.w3.org/1999/xlink}href")))
+            # filename = re.sub("[^/]+", "", img_obj.get("xlink:href"))
+            if filename == None:
+                continue
+
+            matchObj = re.match("([A-Z])-(\d+)_fld_(\d+)_wv_([^.]+)", filename)
+            row = matchObj.group(1)
+            col = matchObj.group(2)
+            # rowi = ord(row) - 65
+            # coli = int(col) - 1
+            fld = matchObj.group(3).zfill(2)
+            # fldi = int(fld) - 1
+            wav = matchObj.group(4)
+            # logger.debug("row = " + row)
+            # logger.debug("col = " + col)
+            # logger.debug("rowi= " + str(rowi))
+            # logger.debug("coli= " + str(coli))
+            # logger.debug("fld = " + fld)
+            # logger.debug("wav = " + wav)
+            images[row] = {} if images.has_key(row) is not None else images[row]
+            images[row][col] = {} if images[row].has_key(col) is not None else images[row][col]
+            images[row][col][fld] = {} if images[row][col].has_key(fld) is not None else images[row][col][fildi]
+            # images[row][col][fld][wav] = img_obj
+            images[row][col][fld][wav] = filename
+
+        logger.debug(pp.pformat(images))
+
+        for rowi, row in sorted(images.items()):
+            # row = images[rowi]
+            for coli, col in sorted(row.items()):
+                # col = images[rowi][coli]
+                for fldi, fld in sorted(col.items()):
+                    # fld = images[rowi][coli][fldi]
+                    for wav, fname in sorted(fld.items()):
+                        logger.debug("wav = " + wav)
+                        logger.debug("filename = " + fname)
+
+
+
+
+        # logger.debug(self.options.angle)
+        # logger.debug(self.options.direction)
+        # logger.debug("self.selectedid = " + str(self.options.ids))
+        # logger.debug("self.getdocids() = " + str(self.getdocids()))
+        # logger.debug("doc_ids = " + str(self.doc_ids))
+        # logger.debug("getElementById(\"image4207\") = " + str(self.getElementById("image4207")))
+        # img_obj = self.getElementById("image4207")
+        # logger.debug(etree.tostring(img_obj))
+        # logger.debug(str(img_obj.get("x")))
+        # img_obj.set("x", "0")
+        # logger.debug(str(img_obj.get("x")))
+        # u = simpletransform.composeParents( img, simpletransform.parseTransform( None ) ) + [[ 0, 0, 1 ]]
 
     # def getMarker( self, ind1, ind2 ):
     #     """
@@ -189,6 +297,19 @@ class ImageAlignEffect( inkex.Effect ):
 
 
 # Create effect instance and apply it.
+logger.debug(len(sys.argv))
+# if len(sys.argv) == 1:
+#     sys.argv = [ './platealign.py', '--angle=0', '--direction=horizontal', '/home/yfujita/work/bin/python/inkscape/platealign/test.svg' ]
+    # inkex.Effect.svg_file = '/home/yfujita/work/bin/python/inkscape/platealign/test.svg'
+
+
+
+
+
+if len(sys.argv) == 1:
+    sys.argv = [ './platealign.py', '--angle=0', '--direction=horizontal', '/home/yfujita/work/bin/python/inkscape/platealign/test.svg' ]
+
 logger.debug( "Started with: {}.".format( str( sys.argv ) ) )
 effect = ImageAlignEffect()
-# effect.affect()
+# effect.affect = new_affect
+effect.affect(args=sys.argv)
